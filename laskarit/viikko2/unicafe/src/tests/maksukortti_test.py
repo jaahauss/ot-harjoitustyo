6c3378864890e3f565965882878d0401d6d6e6db
Jaana Haussalo <145219384+jaahauss@users.noreply.github.com>
2025-03-22 22:26:20 +0200

    Exercises 6 and 7 of week 2 done.

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_kortin_saldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataa_rahaa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+    def test_ota_rahaa_kun_saldo_riittaa(self):
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 9.0)
+
+    def test_ota_rahaa_kun_saldo_ei_riita(self):
+        self.maksukortti.ota_rahaa(2000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_ota_rahaa_true(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100), True)
+
+    def test_ota_rahaa_false(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(2000), False)
